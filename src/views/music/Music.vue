654a111e124f8d25e0e98477bfdbfebc4da1fb1b
yhChen <kchenyuhang@gmail.com>
2020-04-26 19:01:03 +0800

    歌单和歌曲管理

--- src/views/music/Music.vue
+++ src/views/music/Music.vue
@@ -1,99 +1,222 @@
 <template>
-  <div style="padding-top:10px;">
-    <span v-for="(item, index) in menus" :key="index" class="gutter">
-      <mu-button :color="item.icon">{{ item.title }}</mu-button>
-    </span>
-    <v-card>
-      <v-card-title>
-        Nutrition
-        <v-spacer></v-spacer>
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
-      </v-card-title>
-      <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
-    </v-card>
+  <div style="width: 100%">
+    <div>
+      <mu-text-field v-model="keywords" placeholder="search" @blur.prevent="shows"></mu-text-field>
+      <mu-button color="primary" @click="search()">点击搜索</mu-button>
+      <p style="margin:0 0px 0 0px">热门标签:</p>
+      <mu-chip
+        class="demo-chip"
+        v-for="chip in chips"
+        :key="chip.id"
+        :color="chip.type"
+        style="margin:0 20px 0 0;"
+        @click="chipsSearch(chip.name)"
+        >{{ chip.name }}</mu-chip
+      >
+    </div>
+
+    <!-- 左边歌单表 -->
+    <div style="display:flex;padding: 20px 20px">
+      <div class="s-l-table">
+        <mu-data-table :columns="columns" :data="song" class="s-l-table-content">
+          <template slot-scope="scope">
+            <td class="is-left">{{ scope.row.songId }}</td>
+            <td class="is-left">{{ scope.row.songName }}</td>
+            <td class="is-left">{{ scope.row.singer }}</td>
+            <td class="is-left">{{ scope.row.duration }}</td>
+            <td class="is-left">{{ scope.row.createTime.slice(0, 10) }}</td>
+            <td class="is-left">
+              <button type="button" value="删除" style="width:80px;height:30px;background-color:red;">删除</button>
+            </td>
+          </template>
+        </mu-data-table>
+        <mu-flex justify-content="center" v-if="show">
+          <!-- 每页显示的数量 -->
+          <span style="margin-top: 10px">每页显示：</span>
+          <mu-select style="width:70px" v-model="size" full-widt>
+            <mu-option v-for="(option, index) in options" :key="index" :label="option" :value="option"></mu-option>
+          </mu-select>
+          <!-- 分页 -->
+          <mu-pagination :total="totalPage" :current.sync="currentPage"></mu-pagination>
+        </mu-flex>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const initChips = [
+  { id: 1, name: '薛之谦', type: 'secondary' },
+  { id: 2, name: '邓紫棋', type: 'success' },
+  { id: 3, name: '周深', type: 'info' },
+  { id: 4, name: '林俊杰', type: 'warning' },
+  { id: 5, name: '周杰伦', type: 'error' },
+  { id: 6, name: '李荣浩', type: 'primary' }
+]
 export default {
-  name: 'Music',
+  name: 'MusicList',
   data() {
     return {
-      menus: [],
-      search: '',
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
+      chips: [...initChips],
+      sort: {
+        name: '',
+        order: 'asc'
+      },
+      options: ['5', '10', '20', '50'],
+      columns: [
+        { title: 'id', width: 120, name: 'id', align: 'left' },
+        { title: '歌名', name: 'name', width: 180, align: 'left' },
+        { title: '歌手', name: 'singer', width: 150, align: 'left' },
+        { title: '时常', name: 'duration', width: 150, align: 'left' },
+        { title: '创建时间', name: 'create_time', width: 160, align: 'left' },
+        { title: '操作', name: 'del_btn', width: 200, align: 'left' }
       ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        }
-      ]
+      song: [],
+      buttonMenu: [],
+      currentPage: 1,
+      totalPage: 3042,
+      size: 10,
+      active: 0,
+      types: [],
+      typeChildSongList: [],
+      keywords: '',
+      show: true,
+      id: ''
     }
   },
+  components: {},
   created() {
-    let index = this.$route.query.index
-    let index1 = this.$route.query.index1
-    console.log(index, index1)
-    this.menus = JSON.parse(localStorage.getItem('menuList'))[index].subMenus[index1].subMenus
-    console.log(this.menus)
-  }
+    this.getSong()
+    this.currentPage
+    // console.log('token的值' + this.token)
+  },
+  mounted() {},
+  watch: {
+    size: function() {
+      this.getSong()
+    },
+    currentPage: function() {
+      this.getSong()
+    }
+  },
+  methods: {
+    //获取歌曲
+    getSong() {
+      // let roleId = localStorage.getItem('roleId')
+      // console.log(roleId)
+      this.axios({
+        method: 'get',
+        url: this.GLOBAL.baseUrl + '/song/page?',
+        params: {
+          currentPage: this.currentPage,
+          size: this.size
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then((res) => {
+        this.song = res.data.data
+        this.getButtonMenu()
+        console.log(this.types)
+        console.log(this.song)
+      })
+    },
+    chipsSearch(name) {
+      this.keywords = name
+      this.search()
+    },
+    //模糊查询歌单
+    search() {
+      // let roleId = localStorage.getItem('roleId')
+      // console.log(roleId)
+      this.axios({
+        method: 'get',
+        url: this.GLOBAL.baseUrl + '/song/blur?',
+        // 问号带参，表单提交
+        params: {
+          field: this.keywords
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then((res) => {
+        this.song = res.data.data
+        this.show = false
+      })
+    },
+    shows() {
+      if (this.keywords === '') this.show = true
+      this.getSong()
+    },
+    //获取歌单中的按钮权限
+    getButtonMenu() {
+      for (let i = 0; i < this.$store.state.menuList.length; i++) {
+        let menu = this.$store.state.menuList[i].subMenus
+        if (menu != null) {
+          for (let j = 0; j < menu.length; j++) {
+            if (menu[j].title === '歌曲管理') {
+              this.buttonMenu = menu[j].subMenus
+            }
+          }
+        }
+      }
+    }
+  },
+  computed: {}
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.table {
+  background-color: white;
+  color: black;
+}
+.content {
+  flex: 0 0 33%;
+}
+.btn {
+  margin: 20px 20px;
+}
+.s-l-table {
+  width: 150%;
+  background-color: white;
+  margin-right: 60px;
+  box-shadow: 2px 2px 2px 2px #ddd;
+  height: 500px;
+  &-content {
+    height: 550px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    margin-bottom: 10px;
+  }
+}
+.tabs {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 120px;
+  overflow-y: auto;
+}
+.s-l-tab {
+  background-color: white;
+  box-shadow: 2px 2px 2px 2px #ddd;
+  height: 500px;
+  display: flex;
+}
+.childTable {
+  overflow: auto;
+  width: 100%;
+  height: 500px;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+.scroll-hidden {
+  ::-webkit-scrollbar {
+    width: 0 !important;
+  }
+  ::-webkit-scrollbar {
+    width: 0 !important;
+    height: 0;
+  }
+}
+</style>
--- src/views/music/MusicList.vue
+++ src/views/music/MusicList.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card max-width="400" class="mx-auto">
+  <!-- <v-card max-width="400" class="mx-auto">
     <v-container class="pa-1">
       <span v-for="(item, index) in menus" :key="index" class="gutter">
         <mu-button :color="item.icon">{{ item.title }}</mu-button>
@@ -25,7 +25,67 @@
         </v-row>
       </v-item-group>
     </v-container>
-  </v-card>
+  </v-card> -->
+  <div style="width: 100%">
+    <div>
+      <!-- 按钮 -->
+      <mu-button
+        color="primary"
+        class="btn"
+        v-for="(item, index) in buttonMenu"
+        :key="index"
+        @click="Operation(item)"
+        >{{ item.title }}</mu-button
+      >
+      <mu-text-field v-model="keywords" placeholder="search"></mu-text-field>
+      <mu-button color="success" @click="search()">点击搜索</mu-button>
+    </div>
+    <!-- 左边歌单表 -->
+    <div style="display:flex;width: 825px">
+      <div class="s-l-table">
+        <mu-data-table :columns="columns" :data="songList.slice(start, end)" class="s-l-table-content">
+          <template slot-scope="scope">
+            <td class="is-left">{{ scope.row.songListId }}</td>
+            <td class="is-left">{{ scope.row.songListName }}</td>
+            <td class="is-right">{{ scope.row.songCount }}</td>
+            <td class="is-right">{{ scope.row.createTime.slice(0, 10) }}</td>
+          </template>
+        </mu-data-table>
+        <mu-flex justify-content="center">
+          <!-- 每页显示的数量 -->
+          <span style="margin-top: 10px">每页显示：</span>
+          <mu-select style="width:70px" v-model="size" full-widt>
+            <mu-option v-for="(option, index) in options" :key="index" :label="option" :value="option"></mu-option>
+          </mu-select>
+          <!-- 分页 -->
+          <mu-pagination raised circle :total="totalPage" :current.sync="currentPage"></mu-pagination>
+        </mu-flex>
+      </div>
+    </div>
+    <!-- 下边类型表格 -->
+    <div class="s-l-tab" style="margin-top: 20px">
+      <div>
+        <!-- tab标签头 -->
+        <mu-tabs :value.sync="active" class="tabs">
+          <mu-tab v-for="(type, index1) in types" :key="index1">{{ type.type }}</mu-tab>
+        </mu-tabs>
+      </div>
+      <div>
+        <div class="demo-text" v-for="(type, index2) in types" :key="index2" style=" overflow: hidden">
+          <!-- 定义表头信息 -->
+          <mu-data-table :columns="columns" :data="type.child" class="childTable" v-if="active == index2">
+            <!-- 定义表格内的数据 -->
+            <template slot-scope="scope">
+              <td class="is-left">{{ scope.row.song_list_id }}</td>
+              <td class="is-left">{{ scope.row.song_list_name }}</td>
+              <td class="is-right">{{ scope.row.song_count }}</td>
+              <td class="is-right">{{ scope.row.create_time.slice(0, 10) }}</td>
+            </template>
+          </mu-data-table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -33,29 +93,213 @@ export default {
   name: 'MusicList',
   data() {
     return {
-      items: [
-        {
-          src: 'backgrounds/bg.jpg'
-        },
-        {
-          src: 'backgrounds/md.jpg'
-        },
-        {
-          src: 'backgrounds/bg-2.jpg'
-        },
-        {
-          src: 'backgrounds/md2.jpg'
-        }
+      sort: {
+        name: '',
+        order: 'asc'
+      },
+      options: ['5', '10', '15'],
+      columns: [
+        { title: 'id', width: 150, name: 'name', align: 'center' },
+        { title: '名称', name: 'calories', width: 270, align: 'left', sortable: true },
+        { title: '歌曲数', name: 'protein', width: 100, align: 'right', sortable: true },
+        { title: '创建时间', name: 'create_time', width: 190, align: 'right', sortable: true }
       ],
-      menus: []
+      songList: [],
+      buttonMenu: [],
+      currentPage: 1,
+      totalPage: 1000,
+      size: 10,
+      page: 1,
+      active: 0,
+      types: [],
+      typeChildSongList: [],
+      keywords: '',
+      start: 0,
+      end: 10
     }
   },
+  components: {},
   created() {
-    let index = this.$route.query.index
-    let index1 = this.$route.query.index1
-    this.menus = JSON.parse(localStorage.getItem('menuList'))[index].subMenus[index1].subMenus
-  }
+    this.getSongList()
+  },
+  mounted() {},
+  watch: {
+    // 侦听器，监听当前页的数量与当前页值的变化
+    size: function(newSize, oldSize) {
+      this.totalPage = (1000 / this.size) * 10
+      //新值与旧值进行对比
+      if (newSize > oldSize) {
+        this.end = this.end + (newSize - oldSize)
+      } else {
+        this.end = this.end - (oldSize - newSize)
+      }
+    },
+    currentPage: function(newCurrent, oldCurrent) {
+      if (newCurrent == 1000 / this.size) {
+        this.page += 1
+      }
+      if (newCurrent < oldCurrent && newCurrent >= 0) {
+        this.start -= 10
+        this.end -= 10
+      } else {
+        this.start += 10
+        this.end += 10
+      }
+    }
+  },
+  methods: {
+    //导出
+    Operation(item) {
+      this.axios({
+        method: 'get',
+        url: 'http://localhost:8080' + item.path,
+        responseType: 'blob'
+      }).then((res) => {
+        //指定类型位excel表类型
+        const blob = new Blob([res.data], { type: 'application/vnd.ms-excel' })
+        //创建a标签节点
+        const download = document.createElement('a')
+        //与获取验证码url方法一样
+        const href = window.URL.createObjectURL(blob)
+        //给超链接的href属性赋url值
+        download.href = href
+        //设置下载的文件名
+        download.download = '歌单数据表.xls'
+        //将a标签放在body中
+        document.body.appendChild(download)
+        //给a标签生成一个点击事件
+        download.click()
+        //移除a标签，为了下次点击时创建
+        document.body.removeChild(download)
+        //移除url
+        window.URL.revokeObjectURL(href)
+      })
+    },
+    //获取歌单
+    getSongList() {
+      this.axios({
+        method: 'get',
+        url: 'http://localhost:8080/songList/page',
+        params: {
+          currentPage: this.page,
+          size: 3042
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then((res) => {
+        this.songList = res.data.data
+        this.getButtonMenu()
+        this.getSongListType()
+        console.log(this.songList)
+      })
+    },
+    //模糊查询歌单
+    search() {
+      this.axios({
+        method: 'get',
+        url: 'http://localhost:8080/songList/blur',
+        // 问号带参，表单提交
+        params: {
+          // roleId: data.data.admin.roles[0].roleId,
+          field: this.keywords
+        },
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then((res) => {
+        this.songList = res.data.data
+      })
+    },
+    // //翻页
+    // changePage() {
+    //   this.currentPage = this.current
+    //   alert(this.current)
+    // },
+    // //改变每页显示的数据量
+    // changeSelection() {
+    //   this.size = this.selection
+    //   alert(this.selection)
+    // },
+    getSongListType() {
+      this.axios({
+        method: 'get',
+        url: 'http://localhost:8080/songList/type',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then((res) => {
+        //获取各种类型及属于该类型的歌单数据
+        this.types = res.data.data
+        console.log(this.types)
+        //取出第一种类型的所有歌单，作为默认tab页上显示的数据
+        this.typeChildSongList = this.types[0].child
+        console.log(this.typeChildSongList)
+      })
+    },
+    //获取歌单中的按钮权限
+    getButtonMenu() {
+      for (let i = 0; i < this.$store.state.menuList.length; i++) {
+        let menu = this.$store.state.menuList[i].subMenus
+        if (menu != null) {
+          for (let j = 0; j < menu.length; j++) {
+            if (menu[j].title === '歌单管理') {
+              this.buttonMenu = menu[j].subMenus
+            }
+          }
+        }
+      }
+    },
+    //排序
+    handleSortChange({ name, order }) {
+      this.list = this.list.sort((a, b) => (order === 'asc' ? a[name] - b[name] : b[name] - a[name]))
+    }
+  },
+  computed: {}
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.table {
+  background-color: white;
+  color: black;
+}
+.content {
+  flex: 0 0 33%;
+}
+.btn {
+  margin: 20px 20px;
+}
+.s-l-table {
+  width: 100%;
+  background-color: white;
+  margin-right: 60px;
+  box-shadow: 2px 2px 2px 2px #ddd;
+  height: 500px;
+  &-content {
+    height: 450px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    margin-bottom: 10px;
+  }
+}
+.tabs {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 120px;
+  overflow-y: auto;
+}
+.s-l-tab {
+  background-color: white;
+  width: 875px;
+  box-shadow: 2px 2px 2px 2px #ddd;
+  height: 500px;
+  display: flex;
+}
+.childTable {
+  overflow: auto;
+  width: 750px;
+  height: 500px;
+}
+</style>
